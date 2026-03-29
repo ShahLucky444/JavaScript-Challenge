a170a32e57e4b8c8852eb8c0cae94ce9686cfe00
ShahLucky444 <shahlucky755@gmail.com>
2026-03-29 19:18:37 +0530

    learn about var,let,const

--- Day-2-Variable.js
+++ Day-2-Variable.js
@@ -1,8 +1,19 @@
-Name = "shah lucky";
-Age = 19;
-Class = "Sybca";
-Dream = "Fullstack Developer";
-Salary = null;
-isStudy = true;
+// this is not a perfect declaration...
 
-console.log(Name,Age,Class,Dream,Salary,isStudy);
+// Name = "shah lucky";
+// Age = 19;
+// Class = "Sybca";
+// Dream = "Fullstack Developer";
+// Salary = null;
+// isStudy = true; 
+
+// console.log(Name,Age,Class,Dream,Salary,isStudy);
+
+var a = 10;
+console.log(a); // re-declared & can be update // but this is old version // ES-6 2015 (new version) //global
+
+let b =10;
+console.log(b); //can't re-declare & but can update // block
+
+const c = 10;
+console.log(c); //can't re-declare & update // block
