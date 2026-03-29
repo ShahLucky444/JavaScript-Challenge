6bbc3d8508ecd2c8df95384cd49993289422c414
ShahLucky444 <shahlucky755@gmail.com>
2026-03-29 19:19:20 +0530

    learn about datatype in js

---
+++ Day-3-DataType.js
@@ -0,0 +1,21 @@
+//PRIMITIVE DATA TYPE
+let  Name = "lucky"; //string
+let age = 19; //number
+ispass = true; //boolen
+let x; //undefined
+let y = null; //null
+
+console.log(Name,age,ispass,x,y);
+
+//NON PRIMITIVE DATA  TYPE
+// object
+const Student = {
+    name : "shah lucky",
+    age : 19,
+    cgpa :7.8,
+    ispass : true
+};
+
+console.log(Student);
+
+console.log(Student.cgpa + 0.2) ;
