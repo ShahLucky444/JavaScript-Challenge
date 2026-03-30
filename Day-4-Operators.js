4393c6afa5833a24597b0043f004f2af3426521a
ShahLucky444 <shahlucky755@gmail.com>
2026-03-30 21:15:51 +0530

    learn about basic operators

---
+++ Day-4-Operators.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 2;
+
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a*b =",a*b);
+console.log("a/b =",a/b);
+console.log("a%b =",a%b);
+console.log("a**b =",a**b);
+
+// unary  operetor...
+console.log("a++ =",a++);
+console.log(a);
+console.log("a--",a--);
+console.log(a);
+
