7672908a02e387a04f007235771b3fb866ae7f9b
ShahLucky444 <shahlucky755@gmail.com>
2026-03-31 17:15:25 +0530

    learn about loops

---
+++ Day-5-Loops.js
@@ -0,0 +1,11 @@
+//for loop 
+
+/* for(let i=1;i<=5;i++){
+    console.log("Laerning loops in  js ");
+}
+
+for(let j=1;j<=10;j++);{
+    console.log("i am learning javascript");
+} */
+
+// while loop 
