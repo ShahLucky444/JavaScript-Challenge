eeb9d63a0eb134406ffee27d99b489c97d860f1e
ShahLucky444 <shahlucky755@gmail.com>
2026-04-04 19:40:39 +0530

    learn about arrays

---
+++ Day-9-Array.js
@@ -0,0 +1,18 @@
+// let students = ["lucky","bunny","vedvik","andra"];
+// let marks = [76,64,85,95];
+// console.log(students);
+// console.log(marks);
+
+// marks[0] = 82;
+
+// console.log(marks);
+
+//arrays are mutable ..
+
+//for loop using array
+
+let mc = ["loid","light","L","lucky"];
+
+for (let i = 0; i<mc.length; i++);
+
+console.log(mc[i]);
