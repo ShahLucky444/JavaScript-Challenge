8253d8ec08f97233da68838d98b70baef2a071da
ShahLucky444 <shahlucky755@gmail.com>
2026-04-10 20:13:56 +0530

    dom manipulation learning

---
+++ Day-14-Dom1.js
@@ -0,0 +1,8 @@
+let element = document.getElementsByTagName("h1");
+console.log(element);
+
+let parag = document.getElementsByClassName("para");
+console.log(parag);
+
+let h1 = document.getElementById("heading");
+console.log(h1);
