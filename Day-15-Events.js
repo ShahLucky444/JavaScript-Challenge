6e9ced99ba955920d37fa97a88b5ff986d2f1e51
ShahLucky444 <shahlucky755@gmail.com>
2026-04-11 17:55:02 +0530

    learn about events

---
+++ Day-15-Events.js
@@ -0,0 +1,11 @@
+let div = document.querySelector(#div1);
+div.onclick = () => {
+    alert("clicked on h1 tag");
+    console.log("hello");
+};
+
+let btn = document.querySelector(#btn);
+
+btn.onclick = () => {
+    console.log("hello lucky");
+};
