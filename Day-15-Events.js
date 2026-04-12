718083c1c43181dc8e3ad6cf45ede08ff83f4e1f
ShahLucky444 <shahlucky755@gmail.com>
2026-04-12 18:14:47 +0530

    java script is too hard i can't understand anything....(angry)

--- Day-15-Events.js
+++ Day-15-Events.js
@@ -1,11 +1,4 @@
-let div = document.querySelector(#div1);
-div.onclick = () => {
-    alert("clicked on h1 tag");
-    console.log("hello");
-};
-
-let btn = document.querySelector(#btn);
-
-btn.onclick = () => {
-    console.log("hello lucky");
-};
+function clk() {
+  let element = document.getElementById("btn");
+  element.textContent = "helloooww";
+}
