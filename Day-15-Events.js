d594fbfb83bbabd2b4cc485d57a20eb48169285e
ShahLucky444 <shahlucky755@gmail.com>
2026-04-20 20:59:49 +0530

    learn about events in deep

--- Day-15-Events.js
+++ Day-15-Events.js
@@ -1,4 +1,15 @@
-function clk() {
-  let element = document.getElementById("btn");
-  element.textContent = "helloooww";
-}
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.getElementById("div1").innerText = "box is empty";
+});
+
+let heading = document.getElementById("head1");
+
+heading.addEventListener("mouseenter", () => {
+  document.getElementById("head1").style.backgroundColor = "orange";
+});
+
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("dblclick", () => {
+  document.getElementById("head1").style.backgroundColor = "gray";
+});
